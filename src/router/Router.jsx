757edebde714151dfd91ea1646e00c93953e8571
aaliahammedpriom <aaliahammedpriom66@gmail.com>
2024-12-23 00:26:54 +0600

    react router tailwind daisy setup

---
+++ src/router/Router.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+   {
+    path: '/',
+    element: <div>Hellow World</div>
+   }
+])
+export default router
